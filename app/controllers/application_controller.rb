dabf6cb07f2962ea077bf0d28cefd28093dcca2b
IgoVeyner <igorveyner95@gmail.com>
2020-09-17 13:19:07 -0400

    add helper method: redirect_if_logged_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,6 +25,10 @@ class ApplicationController < Sinatra::Base
     def redirect_if_not_logged_in
       redirect '/login' if !logged_in?
     end
+
+    def redirect_if_logged_in
+      redirect '/login' if logged_in?
+    end
   end
 
 end
