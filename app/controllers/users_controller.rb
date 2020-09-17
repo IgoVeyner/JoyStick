fc73f8157fa5b2d42bd3e26c02759af4f6758bef
IgoVeyner <igorveyner95@gmail.com>
2020-09-17 14:07:13 -0400

    edit post '/login' now logs a user in if password valid

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,14 @@ class UsersController < ApplicationController
   end
 
   post '/login' do
+    user = User.find_by(username: params[:user][:username])
     
+    if user && user.authenticate(params[:user][:password])
+      session[:user_id] = user.id
+      redirect "/user/#{user.slug}"
+    else 
+      redirect '/login'
+    end
   end
 
   get '/users/:slug' do
