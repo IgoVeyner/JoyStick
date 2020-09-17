bf0402d85add24c047376d2c5817ee7a8e1d5c9e
IgoVeyner <igorveyner95@gmail.com>
2020-09-17 13:11:07 -0400

    create user controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+
+  get '/signup' do
+  end
+  
+  post '/signup' do
+  end
+
+  get '/login' do
+  end
+
+  post '/login' do
+  end
+
+  get '/users/:slug' do
+  end
+end
