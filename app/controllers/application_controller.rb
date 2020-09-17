a0a35edca071c73adcb07c4c607fa6b7329a19f7
IgoVeyner <igorveyner95@gmail.com>
2020-09-17 13:13:15 -0400

    enable sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "JoyStick 1234sdafASDjklaFDS42LMfj asajj432dsjk37qwFsF234adhZf"
   end
 
   get "/" do
