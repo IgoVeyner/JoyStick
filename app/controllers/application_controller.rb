1dfba1acc2e481f212aafe2e317e9b428c411648
IgoVeyner <igorveyner95@gmail.com>
2020-09-17 13:16:39 -0400

    add helper methods: logged_in?, current_user, redirect_if_not_logged_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,18 @@ class ApplicationController < Sinatra::Base
     erb :welcome
   end
 
+  helpers do
+    def logged_in?
+      !!current_user
+    end
+
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+
+    def redirect_if_not_logged_in
+      redirect '/login' if !logged_in?
+    end
+  end
+
 end
