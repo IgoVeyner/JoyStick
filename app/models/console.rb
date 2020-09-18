662ff298d0c0625587de712ab75abeadb1de9479
IgoVeyner <igorveyner95@gmail.com>
2020-09-18 13:18:31 -0400

    add dependent destroy relationship for consoles has many collections

--- app/models/console.rb
+++ app/models/console.rb
@@ -1,5 +1,5 @@
 class Console < ActiveRecord::Base
-  has_many :collections
+  has_many :collections, dependent: :destroy
   has_many :games, through: :collections
   has_many :users, through: :collections
 
