e967d52662c72d9a72e5d3de150aeb7fe4bf85e1
IgoVeyner <igorveyner95@gmail.com>
2020-09-22 10:59:18 -0400

    add seed file with games

---
+++ db/seeds.rb
@@ -0,0 +1,26 @@
+# Create Consoles
+Console.create(name: "PlayStation", manufacturer: "Sony", release_date: "Dec. 1994")
+Console.create(name: "PlayStation 2", manufacturer: "Sony", release_date: "Mar. 2000")
+Console.create(name: "PlayStation 3", manufacturer: "Sony", release_date: "Nov. 2006")
+Console.create(name: "PlayStation 4", manufacturer: "Sony", release_date: "Nov. 2013")
+Console.create(name: "PlayStation 5", manufacturer: "Sony", release_date: "Nov. 2020")
+
+Console.create(name: "Xbox", manufacturer: "Microsoft", release_date: "Nov. 2001")
+Console.create(name: "Xbox 360", manufacturer: "Microsoft", release_date: "Nov. 2005")
+Console.create(name: "Xbox One", manufacturer: "Microsoft", release_date: "Nov. 2013")
+Console.create(name: "Xbox Series S", manufacturer: "Microsoft", release_date: "Nov. 2020")
+Console.create(name: "Xbox Series X", manufacturer: "Microsoft", release_date: "Nov. 2020")
+
+Console.create(name: "NES", manufacturer: "Nintendo", release_date: "Jul. 1983")
+Console.create(name: "Super NES", manufacturer: "Nintendo", release_date: "Nov. 1990")
+Console.create(name: "N64", manufacturer: "Nintendo", release_date: "Jun. 1996")
+Console.create(name: "GameCube", manufacturer: "Nintendo", release_date: "Nov. 2001")
+Console.create(name: "Wii", manufacturer: "Nintendo", release_date: "Nov. 2005")
+Console.create(name: "Switch", manufacturer: "Nintendo", release_date: "Mar. 2017")
+
+Console.create(name: "Dreamcast", manufacturer: "Sega", release_date: "Nov. 1998")
+Console.create(name: "Saturn", manufacturer: "Sega", release_date: "Nov. 1994")
+
+# Create Games
+# Create Users
+# Create Collections
